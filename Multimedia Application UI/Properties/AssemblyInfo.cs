f190cd48351c4b26adb6ad49853dd3e57b2b36fb
Ahmed Nasser HK <ahmed.play94@gmail.com>
2024-01-05 08:15:05 +0200

    Add files via upload

---
+++ Multimedia Application UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Multimedia Application UI")]
+[assembly: AssemblyDescription("Multimedia Management")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.hk4tech.com")]
+[assembly: AssemblyProduct("Multimedia Application UI")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("HKProjects")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d0e06654-9831-463a-b902-0656ef712e29")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("10.5.2.1")]
+[assembly: AssemblyFileVersion("10.5.2.1")]
